1ba2175be2bab8ce341769bf7cd54b055d40922e
sergey <shkaev.ss@mail.ru>
2022-12-14 15:18:47 +0300

    work on mistakes t1

--- 01/task2.py
+++ 01/task2.py
@@ -4,4 +4,4 @@ print(f'x, y, z')
 for x in range(2):
     for y in range(2):
         for z in range(2):
-            print(f'{x}, {y}, {z}, {not (x or y or z) == (not x) and (not y) and (not z)}')
+            print(f'{x}, {y}, {z}, {(not (x or y or z)) == ((not x) and (not y) and (not z))}')
